82d6e12ea8864d585aff9f2e17ca2291c50b1b3f
SxTer1 <152202044+SxTer1@users.noreply.github.com>
2023-12-14 10:50:39 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ menuCloseButton.addEventListener("click", () => {
 })
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
+    header.classList.remove('header_active');
       e.preventDefault();
 
       const targetId = this.getAttribute('href').substring(1);
